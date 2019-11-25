b9f75901ae66f0aa913fe05b4397a1c3c4017d24
Özay <ozay_ezerceli804@hotmail.com>
2019-11-25 11:22:49 +0300

    structures and templates updated (#11)
    * Use case diagram is updated
    
    ViewEvent, ViewAccount, and PreviewTicket are added.
    
    * Rad update
    
    Use-case definitions for ViewAccount, ViewEvent and PreviewTicket are added to RAD.
    
    * Sequence Diagram and rad updated
    
    * structure and some templates updated

---
+++ Implemantation/ticketsystem/core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
