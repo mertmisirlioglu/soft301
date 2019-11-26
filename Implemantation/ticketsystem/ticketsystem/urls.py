7faf438208851d267594422e860cb1b6450e4441
Özay <ozay_ezerceli804@hotmail.com>
2019-11-26 19:52:56 +0300

    database created

--- Implemantation/ticketsystem/ticketsystem/urls.py
+++ Implemantation/ticketsystem/ticketsystem/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
     path('admin/', admin.site.urls),
 ]
