1fed2b1d58722fc90d42fd7a5e7eba4e07ff8a59
Özay <ozay_ezerceli804@hotmail.com>
2019-11-26 23:34:13 +0300

    login and signup  (#14)
    * Use case diagram is updated
    
    ViewEvent, ViewAccount, and PreviewTicket are added.
    
    * Rad update
    
    Use-case definitions for ViewAccount, ViewEvent and PreviewTicket are added to RAD.
    
    * Sequence Diagram and rad updated
    
    * structure and some templates updated
    
    * database created
    
    * login and signup template updated
    
    * login and signup

--- Implemantation/ticketsystem/website/admin.py
+++ Implemantation/ticketsystem/website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Event, Stage, User, Ticket, Operator, Visitor, Concert, Theatre, Sport
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Stage)
+admin.site.register(User)
+admin.site.register(Ticket)
+admin.site.register(Operator)
+admin.site.register(Visitor)
+admin.site.register(Concert)
+admin.site.register(Theatre)
+admin.site.register(Sport)
+
+
+
