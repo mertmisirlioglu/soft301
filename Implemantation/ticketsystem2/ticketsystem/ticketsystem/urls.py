174e806f519dfb4594b315ad217666c7646f0d3e
Özay <ozay_ezerceli804@hotmail.com>
2019-12-17 04:20:30 +0300

    DatePicker, EditAccount, PreviewTicket updated

--- Implemantation/ticketsystem2/ticketsystem/ticketsystem/urls.py
+++ Implemantation/ticketsystem2/ticketsystem/ticketsystem/urls.py
@@ -15,18 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth.views import PasswordResetDoneView, PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView
 from website import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
+    path('accounts/login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('admin/', admin.site.urls),
     path('event/<int:pk>/preview', views.event_preview, name='event_preview'),
     path('event/<int:pk>/buy', views.ticket_buy_view, name='buy_ticket'),
     path('account/tickets/', views.my_tickets_view, name='my_tickets'),
+    path('account/tickets/<int:pk>/preview', views.ticket_preview, name='ticket_preview'),
     path('account/profile/', views.my_profile_view, name='my_profile'),
-    path('account/profile/edit_event', views.edit_my_profile, name='edit_my_profile'),
+    path('account/profile/edit', views.edit_my_profile, name='edit_my_profile'),
+    path('account/change-password/', views.change_password, name='change_password'),
+    path('account/reset-password/', PasswordResetView.as_view(), name='password_reset'),
+    path('account/reset-password/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('account/reset-password/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('account/reset-password/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
